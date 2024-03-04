6f2a9c13b7d0994c079f361a1c605af1c9959db0
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-04 13:22:05 +0100

    Initial Commit

---
+++ ContactSwiftData/ContactSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactSwiftDataApp.swift
+//  ContactSwiftData
+//
+//  Created by LAANAYA Abderrazak on 4/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
